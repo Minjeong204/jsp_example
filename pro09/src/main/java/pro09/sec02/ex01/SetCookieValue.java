6e2889d2201dcf055fe40681f22bcc98768ed06d
Minjeong204 <anjell1218@gmail.com>
2022-12-12 18:04:14 +0900

    1212

--- pro09/src/main/java/pro09/sec02/ex01/SetCookieValue.java
+++ pro09/src/main/java/pro09/sec02/ex01/SetCookieValue.java
@@ -25,7 +25,8 @@ public class SetCookieValue extends HttpServlet {
 		Date d = new Date();
 		Cookie c = new Cookie("cookieTest", URLEncoder.encode("JSP프로그래밍 입니다.", "utf-8"));
 
-		c.setMaxAge(24 * 60 * 60);
+//		c.setMaxAge(24 * 60 * 60);
+		c.setMaxAge(-1);
 		response.addCookie(c);
 		out.print("현재시간: " + d);
 		out.print("현재시간을 Cookie로 저장합니다.");
---
+++ pro09k/src/main/java/pro09/sec05/ex01/MemberDAO.java
@@ -0,0 +1,51 @@
+package pro09.sec05.ex01;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+
+
+
+public class MemberDAO {
+	private Connection con;
+	private PreparedStatement pstmt;
+	private DataSource dataFactory;
+
+	public MemberDAO() {
+		try {
+			Context ctx = new InitialContext();
+			Context envContext = (Context) ctx.lookup("java:/comp/env");
+			dataFactory = (DataSource) envContext.lookup("jdbc/oracle");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public boolean isExisted(MemberVO memberVO) {
+		boolean result = false;
+		String id = memberVO.getId();
+		String pwd = memberVO.getPwd();
+		try {
+			con = dataFactory.getConnection(); // DataSource를 이용해 데이버베이스에 연결
+			String query = "select decode(count(*), 1, 'true', 'false') as result from t_member";
+			query+= " where id =? and pwd=?";
+			pstmt = con.prepareStatement(query);
+			pstmt.setString(1, id);
+			pstmt.setString(2, pwd);
+			ResultSet rs = pstmt.executeQuery();
+			rs.next();
+			result = Boolean.parseBoolean(rs.getString("result"));
+			System.out.println("result = "+result);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+}
